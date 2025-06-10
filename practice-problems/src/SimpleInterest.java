6fe27de0315b41425373795a10187c266e2d6163
P5V1V3K <113207270+P5V1V3K@users.noreply.github.com>
2025-06-10 09:32:11 +0530

    removed main.java

--- practice-problems/src/SimpleInterest.java
+++ practice-problems/src/SimpleInterest.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class SI {
+public class SimpleInterest {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int p=sc.nextInt();
